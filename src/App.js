08baa85984f14e13a8457045486b416b58db3b0f
Rohan Raghav <132052764+RohanRaghav@users.noreply.github.com>
2025-01-22 23:21:41 +0530

    fixing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Dashboard from './Components/Dashboard';
+import Form from './Components/Form'; // Import the form component
 
 function App() {
   return (
-    <div className="App">
-      <Dashboard />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {/* Route to show the form */}
+          <Route path="/" element={<Form />} />
+
+          {/* Route to show the dashboard */}
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
